972d3d2f4eac88bdc16c7cc87fbb90252efe0b24
agmrezz <agmrezz@gmail.com>
2024-12-02 10:20:04 +0100

    move gitignore out

--- api/main.py
+++ api/main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from loguru import logger
+from fastapi.middleware.cors import CORSMiddleware
 
 from aemet.router import router as antarctic_router
 from database.database import init_db
 from settings.logging import setup_logging
+
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,6 +21,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Axpo Challenge",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(antarctic_router)
